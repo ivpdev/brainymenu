7da1f04aa287a4d6308f213ee5b2e03f4f7a2092
Ivan Panov <ivan.panov@materna.de>
2018-05-20 15:19:27 +0200

    added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
 <template>
 <div class="app thema-red">
-  <f7-statusbar></f7-statusbar>
+  <!-- f7-statusbar></f7-statusbar -->
   <f7-view main>
+    <f7-navbar class="brainymenu-navbar">
+         <f7-nav-left>
+            <f7-link><f7-icon f7="menu"></f7-icon></f7-link>
+          </f7-nav-left>
+
+          <f7-nav-title title="Asia Little Dragon">
+          </f7-nav-title>
+
+          <f7-nav-right>
+              <f7-link panel-open="right"><f7-icon fa="shopping-cart"></f7-icon></f7-link>
+              <f7-badge class="shopping-cart-count" color="red">5</f7-badge>
+          </f7-nav-right>
+    </f7-navbar>
       <!-- Initial Page -->
       <f7-page>
 
-    <Menu v-bind:data="menuData"/>
+<!-- div class="row">
+    <div class="col-50"></div>
+    <div class="col-50">OPA </div>
+</div>
+<div class="row">
+    <div class="col-33">33%</div>
+    <div class="col-33">33%</div>
+    <div class="col-33">33%</div>
+</div -->
+
+<Menu v-bind:data="menuData"/>
+
 
       </f7-page>
   </f7-view>
@@ -16,11 +40,8 @@
 <script>
 import Menu from './components/Menu.vue'
 import menuData from './menuData.json'
-import {  f7Navbar, f7View, f7Page, f7Statusbar } from 'framework7-vue'
-
-
-//console.log('XXX')
-//console.log(menuData)
+import {  f7Navbar, f7NavLeft, f7NavRight, f7NavTitle,
+    f7Link, f7View, f7Page, f7Statusbar, f7Icon, f7Badge } from 'framework7-vue'
 
 export default {
   name: 'app',
@@ -32,13 +53,28 @@ export default {
   components: {
     Menu,
     f7Navbar,
+    f7NavLeft,
+    f7NavRight,
+    f7Link,
     f7View,
     f7Page,
-    f7Statusbar
+    f7Statusbar,
+    f7Icon,
+    f7Badge,
+    f7NavTitle
   }
 }
 </script>
 
 <style>
+.shopping-cart-count {
+    right: 24px;
+    top: -9px;
+    z-index: 1;
+}
+
+.thema-red .brainymenu-navbar.navbar {
+    background-color: #c30808 !important;
+}
 
 </style>
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,6 +1,18 @@
 <template>
-        <f7-list v-bind:media-list="true" class="menu-root">
-            <f7-list-item v-for="(category, index) in data.menu" class="category-li">
+        <f7-list v-bind:media-list="true" v-bind:accordion="true" class="menu-root">
+            <f7-list-item v-for="(category, index) in data.menu" class="category-li"
+                v-on:click="onCategoryHeaderClick(category, index)">
+
+            <!-- f7-list-item accordion-item title="Accordion Item 1">
+                <f7-accordion-content>
+                  <f7-block>
+                    <p>Accordion Item 1 Content</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu felis volutpat, rutrum ex quis, lobortis ex. Curabitur quis mattis lorem. Nullam magna lacus, interdum vel maximus nec, vestibulum non quam. Phasellus ornare efficitur porttitor. Quisque neque diam, imperdiet in fermentum nec, congue vitae ante. Nullam imperdiet maximus commodo. Morbi pharetra id purus ac ultrices. Duis non posuere libero.</p>
+                  </f7-block>
+                </f7-accordion-content>
+              </f7-list-item -->
+
+
                 <li class="list-group-title">{{category.category}}</li>
                 <f7-list-group>
                    <DishListItem v-for="(item, index) in category.items" v-bind:item="item"/>
@@ -12,9 +24,15 @@
 
 <script>
 
-import { f7Card, f7List, f7ListGroup, f7ListItem, f7Button } from 'framework7-vue'
+import { f7Card, f7List, f7ListGroup,
+    f7ListItem, f7Button, f7Accordion, f7AccordionItem,
+    f7AccordionToggle, f7AccordionContent, f7Block } from 'framework7-vue'
 import Framework7 from 'framework7'
 import DishListItem from './DishListItem'
+
+const localState = {
+    collapsed: {}
+}
 
 export default {
   name: 'Menu',
@@ -24,10 +42,29 @@ export default {
     f7ListItem,
     f7Button,
     f7ListGroup,
-    DishListItem
+    DishListItem,
+    f7Accordion,
+    f7AccordionItem,
+    f7AccordionToggle,
+    f7AccordionContent,
+    f7Block
   },
   props: {
     data: Object
+  },
+
+  methods: {
+    isCollapsed: function(categoryIndex) {
+        return localState.collapsed[categoryIndex]
+    },
+
+    onCategoryHeaderClick: function(category, index) {
+        this.toggleCollapsed(index)
+    },
+
+    toggleCollapsed: function(categoryIndex) {
+        localState.collapsed[categoryIndex] = !localState.collapsed[categoryIndex]
+    }
   }
 }
 </script>
@@ -50,8 +87,17 @@ export default {
 }
 
 .thema-red .list-group-title {
-    background: #b70a0a;
+    background: #ce7474;
     color: white;
+}
+
+.chevron-down {
+    background-image: url(data:image/svg+xml;charset=utf-8,%3Csvg%20width%3D'13px'%20height%3D'8px'%2…209%202%209%209%202%209%202%2011%2010%2011'%3E%3C%2Fpolygon%3E%3C%2Fsvg%3E);
+    background-size: 13px 13px;
+}
+
+.list-collapsed {
+    display: none;
 }
 
 </style>
